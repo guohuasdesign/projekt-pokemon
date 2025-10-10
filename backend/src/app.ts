a46bbca72e82ec4b27d80088846746a3be5e2be3
Siri Anandarao <siri.anandarao@attempto.eu>
2025-10-10 11:04:19 +0200

    backend work

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import "./db/index.ts";
+import leaderboardRoutes from "./routers/leaderboardRoutes.ts";
+//import { errorHandler, notFound } from "./middleware/index.ts";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/leaderboard", leaderboardRoutes);
+// app.use('/battle', battleRouter);
+// app.use('/leaderboard', leaderboardRouter);
+// app.use('/search', searchRouter);
+
+//app.use(notFound);
+//app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
