fcfbedb2381b43817f99c22b8c831748b9acfaca
Hua Guo <huaguo0107@gmail.com>
2025-10-10 15:25:16 +0200

    FirstMergen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,46 @@ const App = () => {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<Navigate to="/" />} />
+        {/* <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pokemon/:id"
+          element={
+            <ProtectedRoute>
+              <PokemonDetailsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-roster"
+          element={
+            <ProtectedRoute>
+              <MyRosterPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/battle"
+          element={
+            <ProtectedRoute>
+              <BattlePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/leaderboard"
+          element={
+            <ProtectedRoute>
+              <LeaderboardPage />
+            </ProtectedRoute>
+          }
+        /> */}
       </Routes>
     </BrowserRouter>
   );
